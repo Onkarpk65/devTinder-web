49d946c7b9649305f5345faa1f89f858e7515cf4
Onkarpk65 <onkarpk65@gmail.com>
2025-02-20 19:51:20 +0530

    added feedSlice

--- README.md
+++ README.md
@@ -26,6 +26,7 @@
 - Refactor code to add constants file + create a components store
 - You should not be able to access other routes without login 
 - If token is not present, redirect user to login page 
+- Logout feature
 
 - Body
     - NavBar
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,19 @@
+import axios from "axios"
+import { BASE_URL } from "../utils/constants"
 
 
 const Feed = () => {
+  const getFeed = async () => {
+    try {
+      const res = await axios.get(BASE_URL + '/feed',
+        {withCredentials: true}
+      )
+      console.log(res);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div>Feed</div>
   )
